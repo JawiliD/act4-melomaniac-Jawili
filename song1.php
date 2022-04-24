6cf2b47b9be0d02d32c208b3542454ff6737eb9a
JawiliD <dareajawili@gmail.com>
2022-04-24 13:18:38 +0800

    song 1 updated

--- song1.php
+++ song1.php
@@ -14,43 +14,119 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" style="color: #000" href="index.php"> EUPHORIA </a>
+                <a class="navbar-brand" style="color: #E5E4E4" href="index.php"> EUPHORIA </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse"  id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-dark" href="Song1.php">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-dark" href="Song2.php">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-dark" href="Song3.php">Song 3</a></li>
-                        <li class="nav-item"><a class="nav-link text-dark" href="Song4.php">Song 4</a></li>
-                        <li class="nav-item"><a class="nav-link text-dark" href="Song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song5.php">Song 5</a></li>
                         
                     </ul>
                 </div>
             </div>
         </nav>
-        <!-- Header-->
-        <header class="bg-dark bg-gradient text-white">
-            <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
-                
-            </div>
-        </header>
         <!-- About section-->
-        <section id="songs">
+        <section class="bg-image"
+            style = "background-image: url(images/photo_2.jpg);height: 100vh width: 100vw" id="songs">
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
-                    <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+                    <div class="col-lg-12 text-center" style="color:white;">
+                        <h2 class="text-info">Bones</h2>
+                        <p  class="lead">Imagine Dragons</p>
+                                                
+                        <p>[Verse 1]</p>                    
+                        <p> Gimme, gimme, gimme some time to think </p>                       
+                        <p> I'm in the bathroom, looking at me </p>                        
+                        <p> Face in the mirror is all I need(Ooh-ooh)</p>                     
+                        <p> Waiting 'till the reaper takes my life</p>                        
+                        <p> Never gonna get me out alive</p> 
+                        <p> I will live a thousand million lives (Oooh-ooh)</p>
+                        <br/>
+
+                        <p>Pre-Chorus</p>                        
+                        <p> My patience is waning, Is this entertaining?</p>
+                        <p>Our patience is waning, Is this entertaining?</p>
+                        <br/>
+
+                        <p>[Chorus]</p>                        
+                        <p> I-I-I got this feeling, yeah, you know</p>                        
+                        <p> Where I'm losing all control</p>                        
+                        <p> 'Cause there's magic in my bones</p>                        
+                        <p> I-I-I got this feeling in my soul</p>                        
+                        <p> Go ahead and throw your stones</p>                        
+                        <p> 'Cause there's magic in my bones</p>
+                        <br/>
+
+                        <p> [Verse 2] </p>                        
+                        <p> Playing with a stick of dynamite</p>                        
+                        <p> There was never gray in black and white</p>
+                        <p> There was never wrong 'till there was right (ooh, oh) </p>
+                        <p> Feeling like a boulder hurtling</p>                     
+                        <p> Seeing all the vultures circling</p>                        
+                        <p> Burning in the flames I'm working in</p>                        
+                        <p> Turning in a bed that's darkening</p>
+                        <br/>
+
+                        <p>[Pre-Chorus]</p>                        
+                        <p> My patience is waning, Is this entertaining?</p>
+                        <p> My patience is waning, Is this entertaining?</p>
+                        <br/>
+
+                        <p>[Chorus]</p>                        
+                        <p> I-I-I got this feeling, yeah, you know</p>                        
+                        <p> Where I'm losing all control</p>                        
+                        <p> 'Cause there's magic in my bones</p>                        
+                        <p> I-I-I got this feeling in my soul</p>                        
+                        <p> Go ahead and throw your stones</p>                        
+                        <p> 'Cause there's magic in my bones</p>
+                        <br/>
+
+                        <p>[Post-Chorus]</p>                        
+                        <p> 'Cause there's magic in my bones</p>
+                        <br/>
+
+                        <p>[Bridge]</p>
+                        <p> Look in the mirror of my mind</p>                        
+                        <p> Turning the pages of my life</p>                        
+                        <p> Walking the path so many paced a million times </p>
+                        <p> Drown out the voices in the air</p>                        
+                        <p> Leaving the ones that never cared</p>                        
+                        <p> Picking the pieces up and building to the sky</p>        
+                        <br/>
+
+                        <p>[Pre-Chorus]</p>
+                        <p> My patience is waning, Is this entertaining?</p>
+                        <p> My patience is waning, Is this entertaining?</p>                
+
+                        <p>[Chorus]</p>                        
+                        <p> I-I-I got this feeling, yeah, you know</p>                        
+                        <p> Where I'm losing all control</p>                        
+                        <p> 'Cause there's magic in my bones</p>                        
+                        <p> I-I-I got this feeling in my soul</p>                        
+                        <p> Go ahead and throw your stones</p>                        
+                        <p> 'Cause there's magic in my bones</p>
+                        <br/>
+
+                        <p> [Outro] </p>                        
+                        <p> There goes my mind (I-I-I), don't mind</p>                        
+                        <p> There goes my mind </p>                        
+                        <p> There goes my mind, don't mind</p>                        
+                        <p> There goes my mind</p>
+                        <p> 'Cause there's magic in my bones </p>
+                        <br/>
+
+                        
+                        
                     </div>
                 </div>
             </div>
         </section>
 
-        <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+        <footer class="py-3 bg-info">
+            <div class="container px-4"><p class="m-0 text-center text-dark">Copyright &copy; 2022. Darea Anne M. Jawili</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
