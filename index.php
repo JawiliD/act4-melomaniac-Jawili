035b2e068a14876dfcddabb25af27f8a88c4b34f
JawiliD <dareajawili@gmail.com>
2022-04-24 11:31:09 +0800

    index updated

---
+++ index.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" style="color: #000" href="index.php"> EUPHORIA </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"  id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-dark" href="Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="Song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="Song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="Song5.php">Song 5</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-image"
+                style = "background-image: url(images/photo_1.jpg);height: 100vh">
+
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Euphoria</h1>
+                <p class="fw-bolder">Let music express what you feel</p>
+                <p class="fw-bolder">A home for a diverse genre of music</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: silver;
+   color: white;
+   text-align: center;
+}
+</style>
+        
+
+        <footer class="py-3 bg-success">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Darea Anne M. Jawili</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
